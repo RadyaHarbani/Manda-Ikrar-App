04e99729ee2fcf81981095088d18bfe9069bdc33
RadyaHarbani <radya.i4m@gmail.com>
2024-04-23 22:30:25 +0700

    slicing register page

--- lib/common/routes/app_routes.dart
+++ lib/common/routes/app_routes.dart
@@ -3,9 +3,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const LOGIN_PAGE  = _Paths.LOGIN_PAGE;
+  static const REGISTER_PAGE  = _Paths.REGISTER_PAGE;
 }
 
 abstract class _Paths {
   _Paths._();
   static const LOGIN_PAGE = '/login';
+  static const REGISTER_PAGE = '/register';
 }
